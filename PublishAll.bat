44a3e90a20215041d67ec31568a7763dcc1b0fc6
JyotiSahoo2904 <jyotiaspnet@gmail.com>
2020-11-23 01:54:29 +0530

    Modified

--- PublishAll.bat
+++ PublishAll.bat
@@ -1,1 +1,3 @@
 dotnet publish  -c Release -r win-x64 --self-contained true -o D:\GITRepository\Publish\ -p:PublishReadyToRun=false D:\\GITRepository\\DotNetCore\\WEBAPISERVICE\\WEBAPISERVICE.csproj 
+
+pause
